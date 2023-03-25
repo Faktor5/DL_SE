5a5657e5da796fa9b806a7174cac84990e305ca1
Amin <a.boyaala@outlook.com>
2023-03-25 01:04:14 +0100

    Website DAMN

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 import pandas as pd
+import numpy as np
 import libraries.wikip as wikip
 import libraries.corpus_loader as cl
 import libraries.data_cleaner as dc
@@ -132,14 +133,47 @@ def index():
 @app.route('/search')
 def search():
     query = request.args.get('query')
-    results = search(query)
-    return f"Search results for {query}"
+    print(query)
+    results = round_percent(top_filter(search(query)))
+    return render_template('search.html', title=env["site_name"], query=query, results=results)
+
+@app.route('/article')
+def article():
+    name = request.args.get('name')
+    text = check.article_text_by_name(name)
+    return render_template('article.html', title=env["site_name"], name=name, text=text)
+
 
 #endregion
 
+def round_percent(results):
+    return { k:round(v,2) for k,v in results.items()}
+
+def top_filter(results):
+    return { k:v for k,v in results.items() if v > 0.0}
+
 def search(query):
     cl_query = dc.clean_text(query)
-    v_query = model.transform([cl_query])
+    print(f"Cleaned --{cl_query}--")
+    m_query = model.transform([cl_query])
+    # the vector has for every word in the corpus a value
+    # which is the tfidf value of the word in the query
+    # the tfidf value is the product of the tf value and the idf value
+    # tf value is the frequency of the word in the query
+    # idf value is the inverse document frequency of the word in the corpus
+    # the tfidf value is the importance of the word in the query
+    # the higher the tfidf value, the more important the word is in the query
+    # the tfidf value is the same for every word in the corpus
+    print(f"Vectorized {m_query}")
+    v_query = m_query.toarray().reshape(check.get_shape_word_article_matrix()[0],)
+    print(check.get_shape_word_article_matrix()[0],check.get_shape_word_article_matrix()[1])
+    print(v_query.shape)
+    print(f"Comparable Vector {v_query}")
+    c_matrix = { v[0]:np.dot(v[1], v_query) / np.linalg.norm(v[1]) * np.linalg.norm(v_query) for v in zip(check.article_names(),article_word_matrix.values)}
+    print(f"Comparison Matrix {c_matrix}")
+    cs_matrix = dict(sorted(c_matrix.items(), key=lambda x: x[1], reverse=True))
+    print(f"Sorted Comparison Matrix {cs_matrix}")
+    return cs_matrix
 
 if __name__ == "__main__":
     main()
--- libraries/corpus_filter.py
+++ libraries/corpus_filter.py
@@ -2,11 +2,13 @@ class CorpusFilter:
     articles = None
     words = None
     article_word_matrix = None
+    word_article_matrix = None
 
     def __init__(self, articles, words, article_word_matrix):
         self.articles = articles
         self.words = words
         self.article_word_matrix = article_word_matrix
+        self.word_article_matrix = article_word_matrix.copy().transpose()
         
 
     def article_info(self, index):
@@ -29,8 +31,29 @@ class CorpusFilter:
     def article_text_by_name(self, name):
         return self.articles[self.articles["title"] == name]["article"].iloc[0]
 
+    def article_index_by_name(self, name):
+        return self.articles[self.articles["title"] == name].index[0]
+    
+    def article_url_by_name(self, name):
+        return self.articles[self.articles["title"] == name]["url"].iloc[0]
+
+    def article_vector_by_name(self, name):
+        return self.article_word_matrix.iloc[self.article_index_by_name(name)]
+
     def get_words_by_prefix(self, prefix):
         return list(filter(lambda word: word.startswith(prefix), self.words))
 
     def article_text_by_index(self, index):
         return self.articles["article"].iloc[index]
+
+    def get_shape(self):
+        return self.article_word_matrix.shape[0], self.article_word_matrix.shape[1], len(self.articles)
+
+    def article_names(self):
+        return self.articles["title"].tolist()
+
+    def get_word_article_matrix(self):
+        return self.word_article_matrix
+
+    def get_shape_word_article_matrix(self):
+        return self.word_article_matrix.shape[0], self.word_article_matrix.shape[1]
